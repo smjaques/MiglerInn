970494341c064331b8c968d626203d9d67b399fc
pramika-kumar <pramika.kumar@gmail.com>
2019-12-11 17:23:20 -0800

    Update FR5.java

--- Inn/src/FR5.java
+++ Inn/src/FR5.java
@@ -6,89 +6,89 @@
 
 
 // put all the info in an array; assuming there are empty
-public static String req5(String fName, String lName, String checkIndates, String checkOutdates, String roomCode, String resCode){
+   public static String req5(String fName, String lName, String checkIndates, String checkOutdates, String roomCode, String resCode){
 
-	    String[] inputs = {fName, lName, checkIndates, checkOutdates, roomCode, resCode};
-		int numNull = 0;
+        String[] inputs = {fName, lName, checkIndates, checkOutdates, roomCode, resCode};
+        int numNull = 0;
 
-		for(int i = 0; i < 6; i++){
-			if(i == 3){
-				i++; 
-			}
+        for(int i = 0; i < 6; i++){
+            if(i == 3){
+                i++;
+            }
 
-			if(inputs.get(i) != ""){
-				numNull++;
-			}
-		} 
-		
-		String statement = "SELECT * FROM reservations"; 
-		int i = 0; 
+            if(inputs[i] != ""){
+                numNull++;
+            }
+        }
 
-		if(numNull > 0){
-			statement = statement + " WHERE ";
+        String statement = "SELECT * FROM reservations";
+        int i = 0;
 
-			while(numNull > 0){
-				//First Name 
-				if(i == 0 && inputs.get(i) != ""){ 
-					statement = statement + "FirstName = '" + inputs.get(i) + "'";
-					numNull--;
+        if(numNull > 0){
+            statement = statement + " WHERE ";
 
-					if(numNull > 0){
-						statement = statement + " AND ";
-					}
-				}
+            while(numNull > 0){
+                //First Name
+                if(i == 0 && inputs[i] != ""){
+                    statement = statement + "FirstName = '" + inputs[i] + "'";
+                    numNull--;
 
-				//Last Name
-				else if(i == 1 && inputs.get(i) != ""){
-					statement = statement + "LastName = '" + inputs.get(i) + "'";
-					numNull--;
+                    if(numNull > 0){
+                        statement = statement + " AND ";
+                    }
+                }
 
-					if(numNull > 0){
-						statement = statement + " AND ";
-					}
-				}
+                //Last Name
+                else if(i == 1 && inputs[i]!= ""){
+                    statement = statement + "LastName = '" + inputs[i] + "'";
+                    numNull--;
 
-				//Date Range 
-				else if(i == 2 && inputs.get(i) != ""){
-					statement = statement + "((CheckIn <= '" + inputs.get(i) + "' AND ";
-					statement = statement + "CheckOut >= '" + inputs.get(i) + "') OR ";
+                    if(numNull > 0){
+                        statement = statement + " AND ";
+                    }
+                }
 
-					statement = statement + "(CheckIn >= '" + inputs.get(i) + "' AND ";
-					statement = statement + "CheckIn <= '" + inputs.get(i + 1) + "') OR";
+                //Date Range
+                else if(i == 2 && inputs[i] != ""){
+                    statement = statement + "((CheckIn <= '" + inputs[i] + "' AND ";
+                    statement = statement + "CheckOut >= '" + inputs[i] + "') OR ";
 
-					statement = statement + "(CheckIn >= '" + inputs.get(i) + "' AND ";
-					statement = statement + "CheckOut <= '" + inputs.get(i + 1) + "'))";
+                    statement = statement + "(CheckIn >= '" + inputs[i] + "' AND ";
+                    statement = statement + "CheckIn <= '" + inputs[i] + "') OR";
 
-					numNull--;
-					i++;
+                    statement = statement + "(CheckIn >= '" + inputs[i] + "' AND ";
+                    statement = statement + "CheckOut <= '" + inputs[i+1] + "'))";
 
-					if(numNull > 0){
-						statement = statement + " AND ";
-					}
-				}
+                    numNull--;
+                    i++;
 
-				//Room Code
-				else if(i == 4 && inputs.get(i) != ""){
-					statement = statement + "Room = '" + inputs.get(i) + "'";
-					numNull--;
+                    if(numNull > 0){
+                        statement = statement + " AND ";
+                    }
+                }
 
-					if(numNull > 0){
-						statement = statement + " AND ";
-					}
-				}
+                //Room Code
+                else if(i == 4 && inputs[i] != ""){
+                    statement = statement + "Room = '" + inputs[i] + "'";
+                    numNull--;
 
-				//Reservation Code
-				else if(i == 5 && inputs.get(i) != ""){
-					statement = statement + "Code = '" + inputs.get(i) + "'";
-					numNull--;
+                    if(numNull > 0){
+                        statement = statement + " AND ";
+                    }
+                }
 
-					if(numNull > 0){
-						statement = statement + " AND ";
-					}
-				}
+                //Reservation Code
+                else if(i == 5 && inputs[i] != ""){
+                    statement = statement + "Code = '" + inputs[i] + "'";
+                    numNull--;
 
-				i++;
-			}
-		}
-		return statement + ";";
+                    if(numNull > 0){
+                        statement = statement + " AND ";
+                    }
+                }
+
+                i++;
+            }
+        }
+       return statement + ";";
 	}
